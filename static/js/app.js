010ed38a41739394f5f31d75d002061de1b899b5
Leo <leonardorurrutia@gmail.com>
2021-01-19 18:10:48 -0800

    Added bubble chart and gauge

--- static/js/app.js
+++ static/js/app.js
@@ -1,58 +1,52 @@
 d3.select("#selDataset").on("change", optionChanged)
 
 
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
+// function to initiate the data to html page
 function init() {
     optionChanged()
+    
 }
-// d3.json("../samples.json").then((importedData) => {
-//     var data = importedData;
-//     selectBox = d3.select("#selDataset")
 
-//     importedData.names.forEach(name => {
-//         selectBox.append("option").text(name).attr("value", name)
-//     })
 
-//     console.log(data.samples);
-
-// });
-
+//event handlers action to update graphs and data onto page
 function optionChanged() {
     // d3.event.preventDefault();
 
     d3.json("../samples.json").then((importedData) => {
         var data = importedData;
 
-
         var dropdownMenu = d3.select("#selDataset");
-
-        importedData.names.forEach(name => {
+        
+        data.names.forEach(name => {
             dropdownMenu.append("option").text(name).attr("value", name)
         })
 
         var idLookup = dropdownMenu.property("value");
         
-        var demoInfo = d3.select("#sample-metadata");
-        var barVis = d3.select("#bar");
-
-        // selectBox = d3.select("#selDataset")
-        // importedData.names.forEach(name => {
-        //     selectBox.append("option").text(name).attr("value", name)
-        // })
 
 
-
+        //Creating filtered data from dropdown selection
         var filteredMetadata = data.metadata.filter(person => person.id == idLookup);
-        
-
         var filteredSampleData = data.samples.filter(person => person.id == idLookup);
 
+        
         console.log(filteredSampleData);
 
-        topBacteriaId = filteredSampleData[0].otu_ids.slice(0, 10).reverse();
-        topBacteriaValues = filteredSampleData[0].sample_values.slice(0,10).reverse();
-        topBacteriaLabels = filteredSampleData[0].otu_labels.slice(0,10).reverse();
+        // Data for Bar Graph
+        var topBacteriaId = filteredSampleData[0].otu_ids.slice(0, 10).reverse();
+        var topBacteriaValues = filteredSampleData[0].sample_values.slice(0,10).reverse();
+        var topBacteriaLabels = filteredSampleData[0].otu_labels.slice(0,10).reverse();
+
+        // Data for Bubble Chart
+        var bacteriaID = filteredSampleData[0].otu_ids
+        var bacteriaValues = filteredSampleData[0].sample_values
+        var bacteriaLabels = filteredSampleData[0].otu_labels
+
+        // Data for Gauge
+        var bbWashFreq = filteredMetadata[0].wfreq
+
+        // Demodraphic Info Table
+        var demoInfo = d3.select("#sample-metadata");
 
         demoInfo.html("");
 
@@ -71,10 +65,59 @@ function optionChanged() {
 
         Plotly.newPlot("bar", topOtuData)
 
+
+        // Bubble Chart
+        var bubbleData = [{
+            y: bacteriaValues,
+            x: bacteriaID,
+            text: bacteriaLabels,
+            mode: 'markers',
+            marker: {
+                color: bacteriaID,
+                size: bacteriaValues
+            }
+        }];
+        var layout = {
+            title: 'Marker Size and Color',
+            showlegend: false,
+            height: 600,
+            width: 1200
+        };
+        Plotly.newPlot('bubble', bubbleData, layout);
+
+        // Gauge
+        var data = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: bbWashFreq,
+                title: { text: "Wash Frequency" },
+                type: "indicator",
+                mode: "gauge+number+delta",
+                delta: { reference: 0 },
+                gauge: {
+                    axis: { range: [null, 9]},
+                    steps: [
+                        { range: [0, 1], color: "white" },
+                        { range: [1, 2], color: "darkcyan" },
+                        { range: [2, 3], color: "cyan" },
+                        { range: [3, 4], color: "burlywood" },
+                        { range: [4, 5], color: "coral" },
+                        { range: [5, 6], color: "yellow" },
+                        { range: [6, 7], color: "green" },
+                        { range: [7, 8], color: "blue" },
+                        { range: [8, 9], color: "purple" },
+                    ],
+                    threshold: {
+                        line: { color: "purple", width: 4 },
+                        thickness: 0.75,
+                        value: 4
+                    }
+                }
+            }
+        ];
+        var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+        Plotly.newPlot('gauge', data, layout);
     });
-
-
-
 }
 
 init()
