add6ac8a94f8e2c6829545b0e34df1ca43f8db9d
Leo <leonardorurrutia@gmail.com>
2021-01-19 16:46:17 -0800

    Added init to app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,43 +3,56 @@ d3.select("#selDataset").on("change", optionChanged)
 
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
+function init() {
+    optionChanged()
+}
+// d3.json("../samples.json").then((importedData) => {
+//     var data = importedData;
+//     selectBox = d3.select("#selDataset")
 
-d3.json("../samples.json").then((importedData) => {
-    var data = importedData;
-    selectBox = d3.select("#selDataset")
+//     importedData.names.forEach(name => {
+//         selectBox.append("option").text(name).attr("value", name)
+//     })
 
-    importedData.names.forEach(name => {
-        selectBox.append("option").text(name).attr("value", name)
-    })
+//     console.log(data.samples);
 
-    // console.log(data);
-    // console.log(data.metadata[0].id);
-    // console.log(data.samples);
-
-});
+// });
 
 function optionChanged() {
     // d3.event.preventDefault();
 
     d3.json("../samples.json").then((importedData) => {
+        var data = importedData;
+
 
         var dropdownMenu = d3.select("#selDataset");
+
+        importedData.names.forEach(name => {
+            dropdownMenu.append("option").text(name).attr("value", name)
+        })
+
         var idLookup = dropdownMenu.property("value");
-        var data = importedData;
+        
         var demoInfo = d3.select("#sample-metadata");
         var barVis = d3.select("#bar");
 
+        // selectBox = d3.select("#selDataset")
+        // importedData.names.forEach(name => {
+        //     selectBox.append("option").text(name).attr("value", name)
+        // })
 
-        console.log(idLookup);
+
+
         var filteredMetadata = data.metadata.filter(person => person.id == idLookup);
-        // console.log(filteredMetadata);
-
+        
 
         var filteredSampleData = data.samples.filter(person => person.id == idLookup);
 
+        console.log(filteredSampleData);
+
         topBacteriaId = filteredSampleData[0].otu_ids.slice(0, 10).reverse();
         topBacteriaValues = filteredSampleData[0].sample_values.slice(0,10).reverse();
-
+        topBacteriaLabels = filteredSampleData[0].otu_labels.slice(0,10).reverse();
 
         demoInfo.html("");
 
@@ -50,7 +63,7 @@ function optionChanged() {
         var topOtuData = [{
             x: topBacteriaValues,
             y: topBacteriaId.map(bact => "OTU " + bact),
-            text: topBacteriaId.map(bact => "OTU " + bact),
+            text: topBacteriaLabels,
             name: "OTU",
             type: "bar",
             orientation: "h"
@@ -63,3 +76,5 @@ function optionChanged() {
 
 
 }
+
+init()
